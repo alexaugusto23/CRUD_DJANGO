f23e3aa0b155184b6f88875bbe282eb59d83b7dd
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-16 14:00:08 -0300

    formulario consulta

--- cadastro_loja/views.py
+++ cadastro_loja/views.py
@@ -4,4 +4,13 @@ from cadastro_loja.forms import  Cadastro_lojaForms
 def index(request):
     form =  Cadastro_lojaForms()
     contexto = {'form':form}
-    return render(request,'index.html', contexto)
+    return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    
+    if request.method  == 'POST':
+        form = Cadastro_lojaForms(request.POST)
+        contexto = {'form':form}
+        return render(request, 'consulta.html', contexto)
+    else:
+        return render(request, 'consulta.html')
