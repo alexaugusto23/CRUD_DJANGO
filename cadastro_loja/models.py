c5fe16e85941e6a9f03baee1ab6163e8b9424725
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-19 16:42:50 -0300

    br criado.

--- cadastro_loja/models.py
+++ cadastro_loja/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class TBPessoa(models.Model):
+    nome = models.CharField(max_length=180)
+    celular = models.CharField(max_length=12)
+    data_nascimento = models.DateField()
+    endereco = models.CharField(max_length=180)
+    bairro = models.CharField(max_length=120)
+    cep = models.CharField(max_length=8)
+    referencia = models.CharField(max_length=120)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.name
